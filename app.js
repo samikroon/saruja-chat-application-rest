e558e54241c6e439e3009f92ae9132ff7deb8a34
Sam Kroon <samikroon@gmail.com>
2018-12-06 23:34:14 +0100

    initiial

---
+++ app.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var createError = require('http-errors');
+var express = require('express');
+var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(8989);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+io.on('connection', function (socket) {
+  socket.on('message', function (data) {
+    socket.emit('chat', data);
+  });
+});
+
+module.exports = app;
